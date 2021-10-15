4872368c74a8e6b2339373938b5c0026c92ecb3e
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-10-14 19:37:35 -0500

    Temas, Actividades, Crear Tema Ready

--- src/js/app.js
+++ src/js/app.js
@@ -14,8 +14,23 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
-
 document.getElementById("defaultOpen").click();
+
+//Funcionamiento Docentes Tabs Aside
+
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("main__contenido");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("dropdown-btn");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
 //Funcionamiento Nav
 
@@ -38,6 +53,55 @@ function noActiveNav(){
   if(document.querySelector(".text-adminus")){
     document.querySelector("#tablink-admin").classList.remove("active");
   }
+}
+
+//Agregar Tema
+
+
+function crearTema(){
+  const padre = document.querySelector('#initial');
+  let div = document.createElement("DIV");
+  div.innerHTML += `<div class="">
+
+                      <div class="info__title">
+                          <span>Titulo:</span>
+                          <input type="text" placeholder="Texto" class="input__body">
+                      </div>
+
+                      <div>
+                          <div>
+                              
+                              <label class="switch">
+                                  <input type="checkbox" checked>
+                                  <span class="slider round"></span>
+                              </label>
+
+                              <span>Habilitar cartelera de avisos</span>
+                          </div><!--ToggleButton-->
+                      </div>
+
+                      <div>
+                          <div>
+                              <label class="switch">
+                                  <input type="checkbox" checked>
+                                  <span class="slider round"></span>
+                              </label>
+
+                              <span>Habilitar calendario</span>
+                          </div><!--ToggleButton-->
+                      </div>
+                    </div> <!--Info Crear Tema-->`;
+
+  padre.appendChild(div);
+}
+
+//Cambiar Nombre Tema
+
+function cambiarNombre(){
+  const btnCrear = document.querySelector('#crearTema');
+  let inputText = document.querySelector('#input__body').value;
+  const temaUno = document.querySelector('#tema1');
+  temaUno.textContent = inputText;
 }
 
 
