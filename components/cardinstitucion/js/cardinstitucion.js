2ca5ff40c8f1cfbe846fe54039dafdb24fe7e30f
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-11-17 14:50:41 -0500

    Componente Card Institucion, listo

---
+++ components/cardinstitucion/js/cardinstitucion.js
@@ -0,0 +1,9 @@
+//Componente Sidebar Derecha Admin
+
+function openNavRight() {
+    document.getElementById("mySidenavRight").style.width = "300px";
+}
+  
+function closeNavRight() {
+    document.getElementById("mySidenavRight").style.width = "0";
+}
